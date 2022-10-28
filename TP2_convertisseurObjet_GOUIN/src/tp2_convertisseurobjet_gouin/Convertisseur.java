be7fa627b56b835852c5fa78f2d4471832bc51aa
Robin Gouin <robin.gouin@epfedu.fr>
2022-10-28 14:58:14 +0200

    Fin de la partie 2 du TP2

--- TP2_convertisseurObjet_GOUIN/src/tp2_convertisseurobjet_gouin/Convertisseur.java
+++ TP2_convertisseurObjet_GOUIN/src/tp2_convertisseurobjet_gouin/Convertisseur.java
@@ -18,43 +18,43 @@ public class Convertisseur {
     
     public double CelciusVersKelvin (double CK) {
         double CversK =CK + 273 ;
-        
+        nbConversions += 1;
         return CversK ;
     }
     
     public double KelvinVersCelcius (double KC) {
         double KversC = KC - 273;
-        
+        nbConversions += 1;
         return KversC;
     } 
     
     public double FarenheitVersCelcius (double FC) {
-        double FversC = FC/33.8;
-        
+        double FversC = (FC-32)*5/9;
+        nbConversions += 1;
         return FversC;
     }
     
     public double CelciusVersFarenheit (double CF) {
-        double CversF = CF*33.8;
-        
+        double CversF = (CF*1.8)+32;
+        nbConversions += 1;
         return CversF;
     }
     
     public double FarenheitVersKelvin (double FK) {
         double FversK = (FK/33.8)+273 ;
-        
+        nbConversions += 1;
         return FversK;
     }
     
     public double KelvinVersFarenheit (double KF) { 
         double KversF = (KF-273)*33.8;
-        
+        nbConversions += 1;
         return KversF;
     }
     
     @Override
     public String toString () {
-        return "nb de conversions"+ nbConversions;
+        return "nb de conversions total : "+ nbConversions;
 
     }
     
--- TP2_convertisseurObjet_GOUIN/src/tp2_convertisseurobjet_gouin/TP2_convertisseurObjet_GOUIN.java
+++ TP2_convertisseurObjet_GOUIN/src/tp2_convertisseurobjet_gouin/TP2_convertisseurObjet_GOUIN.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_gouin;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gouin
@@ -14,9 +16,52 @@ public class TP2_convertisseurObjet_GOUIN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         // TODO code application logic here
         
+        Convertisseur premierTest = new Convertisseur ();
+        System.out.println (premierTest.CelciusVersFarenheit(25));
+        System.out.println (premierTest.FarenheitVersCelcius(77));
         
+        Convertisseur secondTest = new Convertisseur ();
+        System.out.println(secondTest.CelciusVersKelvin(30));
+        
+        System.out.println(premierTest.toString());
+        System.out.println(secondTest.toString());
+        
+        System.out.println("\n Quelle conversion voulez vous faire ? \n CK (Celcius vers Kelvin) \n KC (Kelvin vers celcius) \n FC (Farenheit vers Celcius) \n CF (Celcius vers Farenheit) \n FK (Farenheit vers Kelvin) \n KF (Kelvin vers Farenheit)");
+        String s = sc.nextLine();
+        Convertisseur donneeUTILISATEUR = new Convertisseur();
+        if ("CK".equals(s)) {
+            System.out.println("Donnez la valeur à convertir : ");
+            double a = sc.nextDouble();
+            System.out.println(donneeUTILISATEUR.CelciusVersKelvin(a));
+        }
+        if ("FC".equals(s)) {
+            System.out.println("Donnez la valeur à convertir : ");
+            double a = sc.nextDouble();
+            System.out.println(donneeUTILISATEUR.FarenheitVersCelcius(a));
+        }
+        if ("CF".equals(s)) {
+            System.out.println("Donnez la valeur à convertir : ");
+            double a = sc.nextDouble();
+            System.out.println(donneeUTILISATEUR.CelciusVersFarenheit(a));
+        }
+        if ("KC".equals(s)) {
+            System.out.println("Donnez la valeur à convertir : ");
+            double a = sc.nextDouble();
+            System.out.println(donneeUTILISATEUR.KelvinVersCelcius(a));
+        }
+        if ("FK".equals(s)) {
+            System.out.println("Donnez la valeur à convertir : ");
+            double a = sc.nextDouble();
+            System.out.println(donneeUTILISATEUR.FarenheitVersKelvin(a));
+        }
+        if ("KF".equals(s)) {
+            System.out.println("Donnez la valeur à convertir : ");
+            double a = sc.nextDouble();
+            System.out.println(donneeUTILISATEUR.KelvinVersFarenheit(a));
+        }
     }
     
 }
