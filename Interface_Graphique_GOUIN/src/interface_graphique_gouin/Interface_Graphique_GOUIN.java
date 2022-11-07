2fb988be97046b0623516db500681fa30704c3cf
Robin Gouin <robin.gouin@epfedu.fr>
2022-11-07 11:38:23 +0100

    Début TP Interface Graphique Guidé

---
+++ Interface_Graphique_GOUIN/src/interface_graphique_gouin/Interface_Graphique_GOUIN.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interface_graphique_gouin;
+
+/**
+ *
+ * @author gouin
+ */
+public class Interface_Graphique_GOUIN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
