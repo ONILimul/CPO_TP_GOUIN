5f760a99e199b85ad5acf1089245bb8cdec027f1
Robin Gouin <robin.gouin@epfedu.fr>
2022-11-14 09:34:37 +0100

    Début du TP3

---
+++ TP3_Heritage_GOUIN/src/tp3_heritage_gouin/armes.java
@@ -0,0 +1,19 @@
+package tp3_heritage_gouin;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gouin
+ */
+public class armes {
+    String nom;
+    int lvlAttaque; 
+    
+}
+public armes (String unNom, int puissance){
+
+}
