6c243e47061795372ac72e0dcb5116bdd9cd3e5e
Robin Gouin <robin.gouin@epfedu.fr>
2022-11-14 09:53:05 +0100

    Création des classe épée et baton

--- TP3_Heritage_GOUIN/src/tp3_heritage_gouin/TP3_Heritage_GOUIN.java
+++ TP3_Heritage_GOUIN/src/tp3_heritage_gouin/TP3_Heritage_GOUIN.java
@@ -15,6 +15,10 @@ public class TP3_Heritage_GOUIN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        armes arme1 = new armes ("arme de base", 5); //on créer deux armes (une épée et un bâton)
+        armes arme2 = new armes ("arme standard", 6);
+        System.out.println(arme1);
+        System.out.println(arme2);
     }
     
 }
--- TP3_Heritage_GOUIN/src/tp3_heritage_gouin/armes.java
+++ TP3_Heritage_GOUIN/src/tp3_heritage_gouin/armes.java
@@ -10,10 +10,18 @@ package tp3_heritage_gouin;
  * @author gouin
  */
 public class armes {
-    String nom;
+    String nom; //on initialise nos variables pour la classe
     int lvlAttaque; 
     
-}
-public armes (String unNom, int puissance){
+    public armes (String unNom, int puissance) { //on créer un constructeur pour rentrer les attributs des objets plus facilement
 
+    nom = unNom;
+    lvlAttaque = puissance;
+    }
+    @Override
+    public String toString() { //on créer une méthode pour nous afficher les références d'une bière quand on nous le demande
+        String chaine_a_retourner;
+        chaine_a_retourner = nom + "(niveau d'attaque : " + lvlAttaque +")"  ; 
+     return chaine_a_retourner ;
+    }
 }
